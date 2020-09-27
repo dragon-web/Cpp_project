a9ba773d17f9c220c7413a6e4c7ca135360a1dc4
dragon-web <1582943265@qq.com>
2020-09-27 11:02:13 +0800

    Search_tool

--- Search_Tool/Search_Tool/Sysutil.cpp
+++ Search_Tool/Search_Tool/Sysutil.cpp
@@ -1,62 +1,32 @@
 #include"Sysutil.h"
-#include"./sqlite3/sqlite3.h"
+#include"sqlite3.h"
 
 //静态库形式或者是源码形式调用，在程序里面使用数据库
  
-#pragma comment(lib,"./sqlite/sqlite3.lib")
 
-void DirectionList(const string& path, vector<string>& subfile, vector<string> & subdir)
+
+void Test_Sqlite()
 {
-	string _path = path;
-	_path += "\\*.*";
-	_finddata_t file;
-	long handdle = _findfirst(_path.c_str(),&file);
-	if (handdle == -1)
+	sqlite3 *db;
+	int rc = sqlite3_open("test.db", &db);
+	if (rc != SQLITE_OK)
 	{
-		perror("_findfirst");
-		return;
+		printf("Open databases failed\n");
 	}
-	do
+	else
 	{
-		if (strcmp(file.name, ".") == 0 || strcmp(file.name, "..") == 0)
-		{
-			continue;
-		}
-		if (file.attrib & _A_SUBDIR)
-		{
-			/*cout << file.name << endl;
-			string temp_path = path;
-			temp_path += "\\";
-			temp_path += file.name;
-			DirectionList(temp_path);*/
-			subdir.push_back(file.name);
-		}
-		else
-			//cout << file.name << endl;
-			subfile.push_back(file.name);
-	} while (_findnext(handdle,&file) == 0);
+		printf("Open databases succese \n");
+	}
 
-	_findclose(handdle);
+
+	sqlite3_close(db);
 }
 
-void Test_DirectionList()
-{
-	const string& path = "C:\\Users\\飞龙\\Desktop\\BIT C++初阶课件-2019-V4";
-	vector<string> subfile, subdir;
-	DirectionList(path,subfile,subdir);
-	for (const auto &e : subfile)
-	{
-		cout << e << endl;
-	}
-	for (const auto &e : subdir)
-	{
-		cout << e << endl;
-	}
-}
 
 int main()
 {
-	Test_DirectionList();
+	Test_Sqlite();
+
 	system("pause");
 	return 0;
 }
